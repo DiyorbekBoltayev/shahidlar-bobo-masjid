edfc5f2b382f4afd58bd86982691ed3f1171c7f0
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-04-17 10:12:38 +0500

    migratsiya va model yaratildi

--- database/migrations/2022_04_17_050827_create_maqolas_table.php
+++ database/migrations/2022_04_17_050827_create_maqolas_table.php
@@ -13,13 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('maqolas', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('maqolas');
     }
 };
--- database/migrations/2022_04_17_050845_create_ehsons_table.php
+++ database/migrations/2022_04_17_050845_create_ehsons_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('ehsons', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('ehsons');
     }
 };
