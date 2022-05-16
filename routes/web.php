3e84056bc3879196b4aab95deb0e6b76dd8711b9
Fayzulla8387 <xojayevfayzulla7@gmail.com>
2022-05-16 16:21:39 +0500

    sidebar ftbtnhgnt

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\MaqolaController;
 use App\Http\Controllers\EhsonController;
 use App\Http\Controllers\KitobController;
 use App\Http\Controllers\FayzullaController;
+use App\Http\Controllers\ImageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -51,13 +52,7 @@ Route::get('/deletemaqola/{id}',[MaqolaController::class, 'destroy'])->name('del
 
 
 //////////////////////////////////////////////////////////
-Route::get('/addnews',[FayzullaController::class,'addnews'])->name('addnews');
-Route::post('/sendnews',[FayzullaController::class,'create'])->name('create');
-Route::get('/shownews',[FayzullaController::class,'store'])->name('shownews');
-Route::get('/edit/{id}',[FayzullaController::class,'show']);
-Route::post('/update/{id}',[FayzullaController::class,'update']);
-Route::get('/delete/{id}',[FayzullaController::class,'destroy']);
-Route::get('/yangiliksingle/{id}',[FayzullaController::class,'single']);
+Route::resource('photo',ImageController::class);
 
 
 //////////////////////////////////////////////////////////
@@ -88,3 +83,16 @@ Route::get('/editbook/{id}',[KitobController::class,'show']);
 Route::get('/deletebook/{id}',[KitobController::class,'destroy']);
 Route::post('/updatebook{id}',[KitobController::class,'update'])->name('updatebook');
 Route::get('/download/{id}',[KitobController::class,'getDownload'])->name('yukla');
+
+
+
+
+
+///////////////////////////////////////////
+ Route::get('/addnews',[PhotoController::class,'addnews'])->name('addnews');
+Route::post('/sendnews',[FayzullaController::class,'create'])->name('create');
+Route::get('/shownews',[FayzullaController::class,'store'])->name('shownews');
+Route::get('/edit/{id}',[FayzullaController::class,'show']);
+Route::post('/update/{id}',[FayzullaController::class,'update']);
+Route::get('/delete/{id}',[FayzullaController::class,'destroy']);
+Route::get('/yangiliksingle/{id}',[FayzullaController::class,'single']);
