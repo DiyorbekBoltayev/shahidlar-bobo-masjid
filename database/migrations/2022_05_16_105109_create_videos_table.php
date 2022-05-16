d7889405ff8512926415dd59d4b4cd9823dc7ec1
Ozodbek <ozodbekozodov0002@gmail.com>
2022-05-16 16:07:10 +0500

    ol

--- database/migrations/2022_05_16_105109_create_videos_table.php
+++ database/migrations/2022_05_16_105109_create_videos_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('url');
             $table->timestamps();
         });
     }
--- database/migrations/2022_05_16_105155_create_images_table.php
+++ database/migrations/2022_05_16_105155_create_images_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('photo');
             $table->timestamps();
         });
     }
