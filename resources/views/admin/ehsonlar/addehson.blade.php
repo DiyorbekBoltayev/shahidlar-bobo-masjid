a6be6a668b333c41ce580d9cccb6103fd4445a15
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-05-21 12:45:38 +0500

    fixed 2

--- resources/views/admin/ehsonlar/addehson.blade.php
+++ resources/views/admin/ehsonlar/addehson.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.master')
 @section('content')
-    <div class="container" style="background: #ebedf2">
+    <div class="container" style="">
         <h1 class="text-primary text text-center">Ehson qo'shish</h1>
         <div class="w-100">
             <form action="{{url('storeehson')}}" method="post" enctype="multipart/form-data">
@@ -11,7 +11,7 @@
                 </div>
                 <div class="form-floating">
                     <label class="text-primary" for="floatingTextarea2">Ehson haqida batafsil ma'lumot kiriting</label>
-                    <textarea class="form-control" name="text" placeholder="Leave a comment here"  required id="floatingTextarea2" ></textarea>
+                    <textarea class="form-control" name="text" rows="10" placeholder="Batafsil ma'lumot"  required id="floatingTextarea2" ></textarea>
                 </div>
                 <div class="form-floating mb-3">
                     <label class="text text-primary" for="floatingInput">Rasm yuklang</label>
