711d98e6e1885aadb4895c19747994349275850c
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-05-25 15:35:06 +0500

    admin .kitoblar style o'zgartirildi

--- resources/views/admin/kitoblar/editbook.blade.php
+++ resources/views/admin/kitoblar/editbook.blade.php
@@ -2,40 +2,47 @@
 @section('content')
 
     <h1 class="text text-center">Kitobni yangilash </h1>
-    <div class="container d-flex justify-content-center">
-
-        <form action="{{route('updatebook',$data->id)}}" method="post" class="w-50" enctype="multipart/form-data">
+    <div class="container ">
+        <form action="{{url('sendbooks')}}" method="post" enctype="multipart/form-data">
             @csrf
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label ">Book Nomi</label>
-                <input type="text" name="nomi" class="form-control text-black" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{$data->nomi}}" required >
+            <div class="w-100">
+
+                <div class="form-floating mb-3">
+                    <label class="text text-primary" for="floatingInput1"> Nomi </label>
+                    <input type="text" name="nomi" class="form-control" id="floatingInput1" value="{{$data->nomi}}"
+                           required placeholder="Kitob nomini kiriting">
+                </div>
+                <div class="form-floating mb-3">
+                    <label class="text text-primary" for="floatingInput1">Kitob Muallifi </label>
+                    <input type="text" name="muallifi" class="form-control" id="floatingInput1" value="{{$data->avtor}}"
+                           required placeholder="Muallif">
+                </div>
+
+                <div class="row d-flex justify-content-center align-items-center">
+                    <div class="col-12 col-lg-6 col-sm-12 "><img src="/newsimage/{{$data->rasm}}"
+                                                                 style="width: 100%; height: 100%;" alt=""></div>
+                </div>
+                <div class="form-floating mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Rasmni yangilash uchun boshqa rasm
+                        tanlang</label>
+                    <input type="file" name="rasm" class="form-control  text-black">
+                </div>
+                <a class="form-control btn-primary text-center" href="{{route('yukla',$data->id)}}" > Hozirgi faylni yuklab olish </a>
+                <div class="form-floating mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Kitob faylini yangilash uchun boshqa fayl
+                        tanlang</label>
+                    <input type="file" name="kitob" class="form-control  text-black">
+                </div>
+
+                <div class="form-floating mb-3">
+
+                    <button class="btn btn-primary  form-control" type="submit" name="submit"> Saqlash</button>
+
+                </div>
             </div>
-
-            <div class="form-floating">
-                <label for="floatingTextarea2" name="matn" >Book autor </label>
-                <textarea  name="muallifi" class="form-control text-black" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 300px">{{$data->avtor}}"</textarea>
-
-            </div>
-            <br>
-
-            <div class="mb-3">
-                <img style="width: 100px; height: 100px;" src="/newsimage/{{$data->rasm}}" alt="Oldingi rasm">
-            </div>
-
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Rasmni tanlang</label>
-                <input type="file" name="rasm" class="form-control text-black" id="exampleInputPassword1">
-            </div>
-
-             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">File tanlang</label>
-                <input type="file" name="kitob" class="form-control text-black" id="exampleInputPassword1">
-            </div>
-
-
-            <button type="submit" class="btn btn-primary">Yangilash</button>
         </form>
-    </div>
 
+
+    </div>
 
 @endsection
